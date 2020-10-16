e9e0ce0de12ba9011640139b9fae0c7c1d7bef2c
ParkSiHyeon <s.hyeon.dev@gmail.com>
2020-10-16 15:05:29 +0900

    클라이언트 페이지 / 서블릿 페이지 (강아지 선택 - 사진)

---
+++ jsptest/src/jspTest/ChoiceDogServlet.java
@@ -0,0 +1,48 @@
+package jspTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/choice_dog")
+public class ChoiceDogServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProc(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProc(request, response);
+	}
+
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String[] dog=request.getParameterValues("dog");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("</head>");
+		out.println("<body bgcolor='black'>");
+		out.println("<table align='center' bgcolor='yellow'>");
+		out.println("<tr>");
+		for(int i=0; i<dog.length; i++) {
+			out.println("<td>");
+			out.println("<img src=' "+dog[i]+"'/>");	
+			out.println("</td>");
+		}
+		out.println("</tr>");
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
